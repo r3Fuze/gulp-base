e32ffcd5f187c65f0a5d7e61ffb3fa61829a94e1
Morten Lindhardt <r3fuze@gmail.com>
2014-05-06 17:44:50 -0700

    Some more testing

--- test/app-test.js
+++ test/app-test.js
@@ -11,7 +11,7 @@ describe("String", function () {
         it("should replace part of a string", function () {
             var str = "foobar";
 
-            expect( str.replace("bar", "baz") ).to.equal("foobaz");
+            expect(str.replace("bar", "baz")).to.equal("foobaz");
         });
     });
 
@@ -19,7 +19,7 @@ describe("String", function () {
         it("should make strings lowercase", function () {
             var str = "THIS STRING is uPPerCASE";
 
-            expect( str.toLowerCase() ).to.equal("this string is uppercase");
+            expect(str.toLowerCase()).to.equal("this string is uppercase");
         });
     });
 });
